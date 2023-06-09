78aa77ba964043af43568d879e8ed39efd74f46b
Jesse Tomchak <jtomchak@gmail.com>
2023-06-09 14:23:00 -0700

    cap fix

--- Gemfile
+++ Gemfile
@@ -48,6 +48,7 @@ group :development do
   gem "capistrano", "~> 3.17", require: false
   gem 'capistrano-rails', '~> 1.1.0'
   gem 'capistrano-rbenv', '~> 2.2'
+  gem 'capistrano-passenger', '~> 0.2.0'
   gem 'capistrano-bundler'
   gem 'capistrano-secrets-yml'
   # Required
--- config/deploy.rb
+++ config/deploy.rb
@@ -5,9 +5,18 @@ set :application, 'account_relay'
 set :repo_url, 'git@github.com:TheBLVD/AccountRelay.git'
 set :branch, ENV.fetch('BRANCH', 'main')
 
+# Deploy to the user's home directory
+set :deploy_to, "/home/accountrelay/#{fetch :application}"
+
 set :rbenv_type, :user
 set :rbenv_ruby, File.read('.ruby-version').strip
 set :migration_role, :app
+
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system',
+       'public/uploads'
+
+# Only keep the last 5 releases to save disk space
+set :keep_releases, 5
 
 namespace :deploy do
   desc 'Run seed'
