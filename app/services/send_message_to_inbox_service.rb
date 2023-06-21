ddbf47f9da7dfb03623a188420cf8ad468520acc
Jesse Tomchak <jtomchak@gmail.com>
2023-06-21 15:09:18 -0700

    updated HTTP sign with digest 256 https://github.com/jakekara/jakekara.github.io/blob/master/scripts/deliver.rb

--- app/services/send_message_to_inbox_service.rb
+++ app/services/send_message_to_inbox_service.rb
@@ -12,13 +12,24 @@ class SendMessageToInboxService < BaseService
 
     post_message_to_inbox
   end
+  # var digest = CryptographyHelper.GetSHA256Digest(content);
+  # var requestContent = new StringContent(content);
+
+  # requestContent.Headers.Add("Digest", "SHA-256=" + digest);
+
+  # var stringToSign = $"(request-target): post /inbox\ndate: {date.ToString("R")}\nhost: {targetHost}\ndigest: SHA-256={digest}\ncontent-length: {content.Length}";
+  # var signature = CryptographyHelper.Sign(stringToSign);
+  # requestContent.Headers.Add("Signature", $@"keyId=""https://{Config.Instance.Host}/actor#main-key"",algorithm=""rsa-sha256"",headers=""(request-target) date host digest content-length"",signature=""{signature}""");
 
   def post_message_to_inbox
+    sha256 = OpenSSL::Digest.new('SHA256')
+    digest = 'SHA-256=' + Base64.strict_encode64(sha256.digest(@content))
+
     date = Time.now.utc.httpdate
     keypair       = OpenSSL::PKey::RSA.new(ENV['PRIVATE_KEY'])
-    signed_string = "(request-target): post /inbox\nhost: #{@target_host}\ndate: #{date}"
+    signed_string = "(request-target): post /inbox\nhost: #{@target_host}\ndate: #{date}\ndigest: #{digest}"
     signature     = Base64.strict_encode64(keypair.sign(OpenSSL::Digest.new('SHA256'), signed_string))
-    header        = "keyId=\"https://acctrelay.moth.social/actor\",headers=\"(request-target) host date\",signature=\"#{signature}\""
+    header        = "keyId=\"https://acctrelay.moth.social/actor#main-key\",headers=\"(request-target) host date\",signature=\"#{signature}\""
 
     Rails.logger.info "CONTENT: #{@content}"
     Rails.logger.info "TARGET_HOST: #{@target_host}"
@@ -28,7 +39,7 @@ class SendMessageToInboxService < BaseService
     # { 'Host': @relay.to_s, 'Date': date, 'Signature': header, 'Content-Type': 'application/activity+json' }
     Rails.logger.info "Header #{header}"
 
-    response = HTTP.headers('Host': 'staging.moth.social', 'Date': date, 'Signature': header, content_type: 'application/activity+json').post(
+    response = HTTP.headers('Host': 'staging.moth.social', 'Date': date, 'Signature': header, 'Digest': digest).post(
       'https://staging.moth.social/inbox', json: @content
     )
 
