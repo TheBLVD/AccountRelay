63316b2b2ff04ae698b941a3c36cd1c042962c8b
Jesse Tomchak <jtomchak@gmail.com>
2023-06-22 15:18:58 -0700

    revert

--- app/services/send_message_to_inbox_service.rb
+++ app/services/send_message_to_inbox_service.rb
@@ -21,7 +21,7 @@ class SendMessageToInboxService < BaseService
     keypair       = OpenSSL::PKey::RSA.new(ENV['PRIVATE_KEY'])
     signed_string = "(request-target): post /inbox\nhost: staging.moth.social\ndate: #{date}\ndigest: #{digest}"
     signature     = Base64.strict_encode64(keypair.sign(OpenSSL::Digest.new('SHA256'), signed_string))
-    header        = "keyId=\"https://acctrelay.moth.social/actor#main-key\", algorithm=\"rsa-sha256\", headers=\"(request-target) host date digest content-type\", signature=\"#{signature}\""
+    header        = 'keyId="https://acctrelay.moth.social/actor#main-key",headers="(request-target) host date digest",signature="' + signature + '"'
 
     Rails.logger.info "CONTENT: #{@content}"
     Rails.logger.info "TARGET_HOST: #{@target_host}"
