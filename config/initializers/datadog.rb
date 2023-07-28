70c403598b2fe60b7078a1c631189b4dff610ada
Jesse Tomchak <jtomchak@gmail.com>
2023-07-28 09:36:38 -0700

    datadob tracing

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -1,5 +1,22 @@
+require 'datadog/statsd'
 require 'ddtrace'
+require 'logger'
 
 Datadog.configure do |c|
-  c.tracing.instrument :rails, service_name: 'account-relay'
+  c.env = 'production'
+  c.version = '2.5.17'
+  c.service = 'account-relay'
+  c.tracing.sampling.default_rate = 1.0
+  c.profiling.enabled = true
+  c.appsec.enabled = true
+  # Optionally, you can configure the DogStatsD instance used for sending runtime metrics.
+  # DogStatsD is automatically configured with default settings if `dogstatsd-ruby` is available.
+  # You can configure with host and port of Datadog agent; defaults to 'localhost:8125'.
+  c.runtime_metrics.statsd = Datadog::Statsd.new
+end
+
+logger = Logger.new(STDOUT)
+logger.progname = 'my_app'
+logger.formatter = proc do |severity, datetime, progname, msg|
+  "[#{datetime}][#{progname}][#{severity}][#{Datadog::Tracing.log_correlation}] #{msg}\n"
 end
