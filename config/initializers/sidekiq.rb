0501e8cd74e6156c124ec4d72dd15c882df8b6f2
Jesse Tomchak <jtomchak@gmail.com>
2023-08-07 11:55:34 -0700

    log level change

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,11 +1,6 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
-
-  config.logger.level = Logger::WARN
-
-  Rails.logger = Sidekiq.logger
-  ActiveRecord::Base.logger = Sidekiq.logger
-  ActiveJob::Base.logger = Sidekiq.logger
+  config.logger.level = Rails.logger.level
 end
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
