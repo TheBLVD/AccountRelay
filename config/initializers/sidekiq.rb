8b2d130d1c40a3b8cbde4316db946cee58183af5
Jesse Tomchak <jtomchak@gmail.com>
2023-08-07 11:56:31 -0700

    error only

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,7 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
-  config.logger.level = Rails.logger.level
+  # only warning/errors
+  config.logger.level = Logger::WARN
 end
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
