648d310c299c3d913fad128f16243c244f94756e
Jesse Tomchak <jtomchak@gmail.com>
2023-08-22 12:18:40 -0700

    logging

--- app/workers/user_statuses_worker.rb
+++ app/workers/user_statuses_worker.rb
@@ -7,6 +7,7 @@ class UserStatusesWorker
   sidekiq_options queue: 'pull', retry: 0
 
   def perform(user_id)
+    Rails.logger.info "UserStatusesWorker:: #{user_id}"
     FetchUserStatusesService.new.call(user_id)
   end
 end
