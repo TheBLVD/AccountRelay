2373b7e6518183cbce0ddd29028bb77ee17c4622
Jesse Tomchak <jtomchak@gmail.com>
2023-09-05 16:15:04 -0700

    add channel accounts to status updates

---
+++ app/workers/scheduler/update_channel_acct_statuses_scheduler.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'http'
+require 'json'
+
+# Get users that are accounts of any and all Channels
+# Each User is sent to fetch_remote_statuses_service (that will fetch and send the statuses for that account) -workerB
+class Scheduler::UpdateChannelAcctStatusesScheduler
+  include Sidekiq::Worker
+
+  sidekiq_options retry: 0
+
+  def perform
+    # defaults to a 1_000 into redis at a time
+    UserStatusesWorker.perform_bulk(channel_accounts)
+  end
+
+  private
+
+  def channel_accounts
+    ChannelAccount.all.includes(:user).pluck(:user_id, :username, :domain).zip
+  end
+end
