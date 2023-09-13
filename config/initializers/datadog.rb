cb9b610cd36d01a5e63de38509909b1cb559e0d4
Jesse Tomchak <jtomchak@gmail.com>
2023-09-13 14:09:09 -0700

    reduce sampling rate to 0.01

--- config/initializers/datadog.rb
+++ config/initializers/datadog.rb
@@ -6,15 +6,15 @@ Datadog.configure do |c|
   c.env = 'production'
   c.version = '2.5.17'
   c.service = 'account-relay'
-  # c.tracing.enabled = Rails.env.production?
-  # c.tracing.sampling.default_rate = 1.0
-  # c.profiling.enabled = Rails.env.production?
-  # c.appsec.enabled = Rails.env.production?
+  c.tracing.enabled = Rails.env.production?
+  c.tracing.sampling.default_rate = 0.01
+  c.profiling.enabled = Rails.env.production?
+  c.appsec.enabled = Rails.env.production?
   c.runtime_metrics.enabled = Rails.env.production?
   # Optionally, you can configure the DogStatsD instance used for sending runtime metrics.
   # DogStatsD is automatically configured with default settings if `dogstatsd-ruby` is available.
   # You can configure with host and port of Datadog agent; defaults to 'localhost:8125'.
-  # c.runtime_metrics.statsd = Datadog::Statsd.new
+  c.runtime_metrics.statsd = Datadog::Statsd.new
 end
 
 logger = Logger.new(STDOUT)
